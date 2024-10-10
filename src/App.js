65c9bee1658573778b6dd698b2cfa6fbc3a52afd
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-10 18:18:32 +0100

    Search feature implementation is done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./Components/MovieList";
@@ -9,6 +9,17 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?i=tt3896198&apikey=f1cce424&s=${searchValue}`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center my-4">
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -2,7 +2,7 @@ const MovieList = ({ movies }) => {
   return (
     <>
       {movies.map(({ Poster }) => (
-        <div className="image-container d-flext justify-content-start m-1">
+        <div className="image-container d-flex justify-content-start m-1">
           <img src={Poster} alt="movie" />
         </div>
       ))}
